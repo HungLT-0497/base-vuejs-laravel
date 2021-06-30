5424e19f44546ddae5555c2b44a1aafa1292b769
HungLt-0497 <hunglt@funtap.vn>
2021-06-30 11:15:30 +0700

    update

--- base/lib/base/Component/Index.vue
+++ base/lib/base/Component/Index.vue
@@ -7,19 +7,16 @@
             <FDetail v-if="detail" :detail="detail" :config_detail="config_detail" :detail_value="detail_value"
                      :visible="visible_detail" @set_visible="() => visible_detail = !visible_detail"></FDetail>
             <FFilter v-if="filter" :filter="filter" :config_filter="config_filter" @change_filter="(data) => {filter_setup = data }"></FFilter>
-            <EditGameConfig/>
     </div>
 </template>
 <script>
 import FTable from './Table/Table'
 import FDetail from './Detail'
 import FFilter from './Filter'
-import EditGameConfig from './../../components/gameconfig/EditGameConfig'
 import Table from "../JS/Utils/Table";
 export default {
     components: {
         FTable,
-        EditGameConfig,
         FDetail,
         FFilter
     },
